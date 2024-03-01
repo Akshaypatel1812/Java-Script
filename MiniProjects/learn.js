c15d300312f43181fc67abfe9339fec5918bc5ab
Akshaypatel1812 <pak180301@gmail.com>
2024-03-01 16:56:05 +0530

    learn DOM & eventListners

---
+++ MiniProjects/learn.js
@@ -0,0 +1,12 @@
+// script.js
+// learn.js
+document.addEventListener('DOMContentLoaded', function() {
+    let cont = document.querySelector('.container');
+    let body = document.body;
+
+    cont.addEventListener('click', function() {
+        body.style.backgroundColor = 'white';
+    });
+});
+
+
